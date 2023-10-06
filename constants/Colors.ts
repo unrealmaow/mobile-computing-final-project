98bcd7b2b31294016f3a5bb092ce878aae1078a9
Sameel Butt <officialsameel@gmail.com>
2023-10-06 15:05:52 +0500

    INTIAL PUSH

---
+++ constants/Colors.ts
@@ -0,0 +1,25 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+  primary: '#FF5F5D',
+  lightGrey: '#FCF8FF',
+  grey: '#EEE9F0',
+  medium: '#747E7E',
+  mediumDark: '#3F7C85',
+  green: '#00CCBF',
+};
